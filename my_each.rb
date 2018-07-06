9c9021b1e20112a5a8d6d57e5a296536a85d1291
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-06 14:30:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,11 +2,11 @@
 
 def my_each(array)
   i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  array
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
 end
 
 #my_each(data) {|i| puts i}
